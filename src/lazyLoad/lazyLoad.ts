c753e8f6e25338a49fb05eca0cc4f529544f2e24
Alsmile <Alsmile123@qq.com>
2017-02-12 22:40:58 +0800

    add lazyload

---
+++ src/lazyLoad/lazyLoad.ts
@@ -0,0 +1,45 @@
+import { Directive, Input, Renderer, ElementRef } from '@angular/core';
+import {Observable, Subscription} from "rxjs/Rx";
+
+@Directive({
+  selector: '[imageLazyLoad]'
+})
+export class ImageLazyLoad {
+  @Input('imageLazyLoad') url: string;
+  @Input('imageLazyLoadParams') params: string;
+  @Input() threshold: number = 0;
+  private scrollSubscription: Subscription;
+  constructor(private el: ElementRef, private renderer: Renderer) {
+    let scrollStream = Observable.fromEvent(window, 'scroll').debounceTime(100);
+    this.scrollSubscription = scrollStream.subscribe(() => {
+      this.loadInView();
+    });
+  }
+
+  ngOnInit () {
+    this.loadInView();
+  }
+
+  loadInView():void {
+    let ePos = this.getPosition();
+    if (ePos.bottom > 0 && (ePos.bottom >= (window.pageYOffset - this.threshold)) && (ePos.top <= ((window.pageYOffset + window.innerHeight) + this.threshold))) {
+      this.setImage();
+    }
+  }
+
+  getPosition() {
+    let box = this.el.nativeElement.getBoundingClientRect();
+    let top = box.top + (window.pageYOffset - document.documentElement.clientTop);
+    return {
+      top: top,
+      left: box.left + (window.pageXOffset - document.documentElement.clientLeft),
+      bottom: top + this.el.nativeElement.clientHeight
+    };
+  }
+
+  setImage() {
+    this.scrollSubscription.unsubscribe();
+    this.scrollSubscription = undefined;
+    this.renderer.setElementAttribute(this.el.nativeElement, 'src', this.url + this.params);
+  }
+}
