2d7eff97e43a5b6eb4d1b1595ada69dce107b4aa
Alsmile <Alsmile123@qq.com>
2017-03-28 23:54:01 +0800

    update for ngMoudle

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-#le5le-components
-le5le-components是le5le的公用angular2组件。
+# le5le-components  
+le5le-components是le5le的公用angular组件。
 
-使用教程，参考 <a href="https://le5le-com.github.io/le5le-components" target="_blank">le5le-components官网</a>
+使用教程，参考:  
+<a href="https://le5le-com.github.io/le5le-components" target="_blank">le5le-components官网</a>
+<a href="https://github.com/le5le-com/le5le-ng-start" target="_blank">le5le-ng-start</a>
+
 
 组件有：  
 + 消息通知notice  
@@ -15,26 +18,110 @@ le5le-components是le5le的公用angular2组件。
 
 + 文字过长省略号显示ellipsis
 
-#使用
+#  使用
 npm install le5le-components
-然后按需import即可
+### 1. 按需import  
+```$xslt
+import {
+  ImageLazyLoad,
+  EllipsisPipe,
+  SwitchComponent,
+  QRCodeComponent,
+  PhoneValidator,
+  PasswordValidator,
+  PositiveIntegerValidator,
+  SameValidator,
+  EmailValidator,
+  FileSelectDirective,
+  FileUploadComponent,
+  BtnSavingDirective,
+  TouchFormDirective,
+  EditorComponent,
+  NoticeService 
+} from "le5le-components";
 
-##目录结构  
+@NgModule({
+  imports:       [
+    CommonModule,
+    FormsModule,
+  ],
+  declarations: [
+    ImageLazyLoad,
+    EllipsisPipe,
+    SwitchComponent,
+    QRCodeComponent,
+    PhoneValidator,
+    PasswordValidator,
+    PositiveIntegerValidator,
+    SameValidator,
+    EmailValidator,
+    FileSelectDirective,
+    FileUploadComponent,
+    BtnSavingDirective,
+    TouchFormDirective,
+    EditorComponent,
+  ],
+  exports:       [
+    CommonModule,
+    FormsModule,
+    ImageLazyLoad,
+    EllipsisPipe,
+    SwitchComponent,
+    QRCodeComponent,
+    PhoneValidator,
+    PasswordValidator,
+    PositiveIntegerValidator,
+    SameValidator,
+    EmailValidator,
+    FileSelectDirective,
+    FileUploadComponent,
+    BtnSavingDirective,
+    TouchFormDirective,
+    EditorComponent,
+  ],
+  providers:     [ NoticeService ]
+})
+export class SharedModule {
+}
+```
+
+
+### 2. 导入整个模块  
+```$xslt
+import { Le5leComponentsModule } from "le5le-components";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    Le5leComponentsModule,
+  ],
+  declarations: [ ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    Le5leComponentsModule,
+  ],
+  providers: [ ]
+})
+export class SharedModule {
+}
+
+```
+
+## 目录结构  
 
 --src 组件源码  
 --demo 帮助文档源码  
 --docs github网页
 
 
-#demo
-demo是使用帮助网站 - le5le-components官网，同时也是开发参考用法。
 
-先npm install安装依赖包后，再运行npm start命令即可进行本地开发调试demo。
+## 本地调试 
+本地开发调试： npm start运行demo
 
-## 编译打包
+## 编译  
 生产环境中，需要用npm run build编译github的docs网站文件。
-
-
 
 
 
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { NoticeService } from './notice/notice.service';
+import { ImageLazyLoad } from './lazyLoad/imgLazyLoad';
+import { EllipsisPipe } from './pipe/ellipsis.pipe';
+import { SwitchComponent } from './switch/switch.component';
+import { QRCodeComponent } from './qrcode/qrcode.component';
+import { PhoneValidator } from './validator/phone.directive';
+import { PasswordValidator } from './validator/password.directive';
+import { PositiveIntegerValidator } from './validator/positiveInteger.directive';
+import { SameValidator } from './validator/same.directive';
+import { EmailValidator } from './validator/email.directive';
+import { FileSelectDirective } from './fileUpload/fileSelect.directive';
+import { FileUploadComponent } from './fileUpload/fileUpload.component';
+import { EditorComponent } from './editor/editor.component';
+import { BtnSavingDirective } from './form/btnSaving.directive';
+import { TouchFormDirective } from './form/touchForm.directive';
+
+export { NoticeService } from './notice/notice.service';
+export { ImageLazyLoad } from './lazyLoad/imgLazyLoad';
+export { EllipsisPipe } from './pipe/ellipsis.pipe';
+export { SwitchComponent } from './switch/switch.component';
+export { QRCodeComponent } from './qrcode/qrcode.component';
+export { PhoneValidator } from './validator/phone.directive';
+export { PasswordValidator } from './validator/password.directive';
+export { PositiveIntegerValidator } from './validator/positiveInteger.directive';
+export { SameValidator } from './validator/same.directive';
+export { EmailValidator } from './validator/email.directive';
+export { FileSelectDirective } from './fileUpload/fileSelect.directive';
+export { FileUploadComponent } from './fileUpload/fileUpload.component';
+export { EditorComponent } from './editor/editor.component';
+export { BtnSavingDirective } from './form/btnSaving.directive';
+export { TouchFormDirective } from './form/touchForm.directive';
+
+const MODULES = [
+  ImageLazyLoad,
+  EllipsisPipe,
+  SwitchComponent,
+  QRCodeComponent,
+  PhoneValidator,
+  PasswordValidator,
+  PositiveIntegerValidator,
+  SameValidator,
+  EmailValidator,
+  FileSelectDirective,
+  FileUploadComponent,
+  EditorComponent,
+  BtnSavingDirective,
+  TouchFormDirective,
+];
+
+@NgModule({
+  imports: [CommonModule, FormsModule],
+  declarations: MODULES,
+  exports: MODULES,
+  providers: [NoticeService]
+})
+export class Le5leComponentsModule {
+}
