fc087272492ece90d7768d6b786628953f64d5e2
alsmile <alsmile123@qq.com>
2017-06-20 20:05:37 +0800

    touch form

--- src/form/touchForm.directive.ts
+++ src/form/touchForm.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, HostListener, Renderer, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Directive({
@@ -6,13 +6,15 @@ import { NgForm } from '@angular/forms';
 })
 export class TouchFormDirective {
   @Input() touchForm: NgForm;
-  constructor() {
+  constructor(private elementRef: ElementRef, private renderer: Renderer) {
   }
 
   @HostListener('submit') onSubmit() {
     for (let i in this.touchForm.controls) {
       this.touchForm.controls[i].markAsTouched();
     }
+
+    this.renderer.setElementClass(this.elementRef.nativeElement, 'form-untouched', false);
   }
 
 }
