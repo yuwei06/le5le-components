b8234f010dad819eb9d4ba24d93047f88d1b4c72
Alsmile <alsmile123@qq.com>
2017-08-16 19:45:28 +0800

    vs code editor

--- src/code/code.component.ts
+++ src/code/code.component.ts
@@ -1,166 +1,117 @@
-import { Component, Input, forwardRef, ElementRef } from '@angular/core';
+import { Component, Input, forwardRef, ElementRef, OnInit, AfterViewInit, ViewChild, Output, EventEmitter } from '@angular/core';
 import { AbstractControl, ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-enum CodeType { default, reservedWords, string, num, time, href };
+declare const monaco: any;
+declare const require: any;
 
 @Component({
   selector: 'ui-code',
-  template: `
-    <div class="ui-code {{options.theme}}" [class.margin3]="rows.length>99" [class.margin4]="rows.length>999">
-      <div class="margin">
-        <div *ngFor="let row of rows; let i=index">{{i+1}}</div>
-      </div>
-      <div class="text" [attr.contenteditable]="!readonly" (blur)="onBlur()" (keydown)="onKeyDown($event)">
-        <div *ngFor="let row of rows; let i=index" class="row row{{i}}">
-          <div class="margin-num" contenteditable="false">{{i+1}}</div>
-          <ng-template ngFor let-col let-j="index" [ngForOf]="row" >
-            <span class="default grid{{i}}{{j}}" *ngIf="col.type===0 && col.text">{{col.text}}</span>
-            <span class="default grid{{i}}{{j}}" *ngIf="col.type===0 && !col.text">&nbsp;</span>
-            <span class="reserved grid{{i}}{{j}}" *ngIf="col.type===1">{{col.text}}</span>
-            <span class="string grid{{i}}{{j}}" *ngIf="col.type===2">{{col.text}}</span>
-            <span class="num grid{{i}}{{j}}" *ngIf="col.type===3">{{col.text}}</span>
-            <span class="time grid{{i}}{{j}}" *ngIf="col.type===4">{{col.text}}</span>
-            <span class="href grid{{i}}{{j}}" *ngIf="col.type===5">{{col.text}}</span>
-          </ng-template>
-        </div>
-      </div>
-    </div>
-  `,
+  template: `<div #editor class="ui-code"></div>`,
   providers: [{
     provide: NG_VALUE_ACCESSOR,
     useExisting: forwardRef(() => CodeComponent),
     multi: true
   }],
 })
-export class CodeComponent implements ControlValueAccessor {
-  private val: any;
+export class CodeComponent implements OnInit, AfterViewInit {
+  @ViewChild('editor') editorContent: ElementRef;
   @Input() options: any = {};
-  @Input() readonly: boolean = false;
-  @Input() required: boolean = false;
-  valueChange: (value: any) => void = () => { };
-  rows: any[] = [];
-  constructor(private _elemRef: ElementRef) {
+  @Output() change = new EventEmitter();
+
+  editor: any;
+  private _value = '';
+  constructor() {
+
+  }
+
+  get value(): string { return this._value; };
+  set value(v: string) {
+    if (v !== this._value) {
+      this._value = v;
+      this.onChange(v);
+    }
   }
 
   ngOnInit() {
-    if (!this.options.reservedWords) {
-      this.options.reservedWords = ["if", "then", "do", "else", "elif", "while", "until", "for", "in",
-        "esac", "fi", "fin", "", "fil", "done", "exit", "set", "unset", "export", "function"];
+  }
+
+  ngAfterViewInit() {
+    let onGotAmdLoader = () => {
+      // Load monaco
+      (<any>window).require.config({ paths: { 'vs': 'assets/monaco/vs' } });
+      (<any>window).require(['vs/editor/editor.main'], () => {
+        this.initMonaco();
+      });
+    };
+
+    // Load AMD loader if necessary
+    if (!(<any>window).require) {
+      let loaderScript = document.createElement('script');
+      loaderScript.type = 'text/javascript';
+      loaderScript.src = 'assets/monaco/vs/loader.js';
+      loaderScript.addEventListener('load', onGotAmdLoader);
+      document.body.appendChild(loaderScript);
+    } else {
+      onGotAmdLoader();
     }
   }
 
-  append(row: string, index?: number) {
-    if (index === undefined) this.rows.push(this.parseRow(row));
-    else this.rows.splice(index, 0, this.parseRow(row));
+  /**
+   * Upon destruction of the component we make sure to dispose both the editor and the extra libs that we might've loaded
+   */
+  ngOnDestroy() {
+    this.editor.dispose();
   }
 
-  delete(index: number, count: number) {
-    this.rows.splice(index, count);
+  // Will be called once monaco library is available
+  initMonaco() {
+    let options = this.options;
+    options.value = this._value;
+    this.editor = monaco.editor.create(this.editorContent.nativeElement, options);
+
+    // Currently setting this option prevents the autocomplete selection with the "Enter" key
+    // TODO make sure to propagate the event to the autocomplete
+    if (this.options.customPreventCarriageReturn === true) {
+      let preventCarriageReturn = this.editor.addCommand(monaco.KeyCode.Enter, function () {
+        return false;
+      });
+    }
+
+    this.editor.getModel().onDidChangeContent((e) => {
+      this.updateValue(this.editor.getModel().getValue());
+    });
   }
 
-  get value(): any { return this.val; };
+  /**
+   * UpdateValue
+   *
+   * @param value
+   */
+  updateValue(value: string) {
+    this.value = value;
+    this.onChange(value);
+    this.onTouched();
+    this.change.emit(value);
+  }
 
-  set value(v: any) {
-    if (v && v !== this.val) {
-      this.valueChange(this.val);
+  /**
+   * WriteValue
+   * Implements ControlValueAccessor
+   *
+   * @param value
+   */
+  writeValue(value: string) {
+    this._value = value || '';
+    if (this.editor) {
+      this.editor.getModel().setValue(this._value);
     }
   }
 
-  writeValue(value: any) {
-    this.val = value;
-    if (value) {
-      let rowObjs: any[] = [];
-      let rows = value.split('\n');
-      for (let row of rows) {
-        rowObjs.push(this.parseRow(row));
-      }
-      this.rows = rowObjs;
-    }
-  }
+  onChange(_) { }
+  onTouched() { }
+  registerOnChange(fn) { this.onChange = fn; }
+  registerOnTouched(fn) { this.onTouched = fn; }
 
-  registerOnChange(fn: any) {
-    this.valueChange = fn;
-  }
-
-  registerOnTouched(fn: any) {
-  }
-
-  isReservedWords(word: string) {
-    for (let item of this.options.reservedWords) {
-      if (word === item) return true;
-    }
-
-    return false;
-  }
-
-  isString(word: string) {
-    if (word.length > 1 && word[0] === "'" && word[word.length - 1] === "'") return true;
-    if (word.length > 1 && word[0] === '"' && word[word.length - 1] === '"') return true;
-
-    return false;
-  }
-
-  isNum(word: string) {
-    return /^[+-]?[0-9]+.?[0-9]*$/.test(word);
-  }
-
-  isTime(word: string) {
-    if (/\d{4}(\-|\/|.)\d{1,2}\1\d{1,2}/.test(word)) return true;
-    else if (/(?:(?:[0-2][0-3])|(?:[0-1][0-9])):[0-5][0-9]:[0-5][0-9]/.test(word)) return true;
-
-    return false;
-  }
-
-  isHref(word: string) {
-    return /^((https|http)?:\/\/)[^\s]+/.test(word);
-  }
-
-  parseRow(text: string) {
-    let colObjs: any[] = [];
-    let cols = text.split(' ');
-    for (let col of cols) {
-      let v: any = { type: CodeType.default, text: col };
-      if (!col) v.text = '';
-      else if (this.isReservedWords(col)) v.type = CodeType.reservedWords;
-      else if (this.isString(col)) v.type = CodeType.string;
-      else if (this.isNum(col)) v.type = CodeType.num;
-      else if (this.isTime(col)) v.type = CodeType.time;
-      else if (this.isHref(col)) v.type = CodeType.href;
-
-      colObjs.push(v);
-    }
-    return colObjs;
-  }
-
-  getContent(): string {
-    let text: string = '';
-    let elems = this._elemRef.nativeElement.querySelectorAll('.text>div');
-    for (let i = 0; i < elems.length; ++i) {
-      let str: string = elems[i].innerText;
-      text += str.replace((i + 1) + '', '');
-    }
-    return text;
-  }
-
-  onBlur() {
-    this.val = this.getContent();
-    this.valueChange(this.val);
-  }
-
-  onKeyDown(event: any) {
-    let selection: any = window.getSelection();
-    console.info(event, selection, selection.focusNode.textContent)
-    if (selection.focusNode.parentNode.nodeName === 'SPAN') {
-
-    }
-    // return false;
-
-    // setTimeout(() => {
-    //   console.info(this.getContent())
-    //   this.writeValue(this.getContent());
-    //   this.valueChange(this.val);
-    // }, 200);
-  }
 }
 
 require('./code.pcss');
