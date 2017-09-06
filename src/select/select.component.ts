f62254e512f8b5c62d160d49499ffaa3faa0639d
Alsmile <alsmile123@qq.com>
2017-09-06 17:31:18 +0800

    select 优化

--- src/select/select.component.ts
+++ src/select/select.component.ts
@@ -1,26 +1,28 @@
-import { Component, Input, forwardRef, ElementRef } from '@angular/core';
+import { Component, Input, forwardRef, ElementRef, Output, EventEmitter } from '@angular/core';
 import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR, Validator } from '@angular/forms';
 
 @Component({
   selector: 'ui-select',
   template: `
-    <div class="ui-select input" [class.readonly]="readonly" (click)="onClick()" title="单击下拉，支持多选">
+    <div class="ui-select input" [class.readonly]="readonly" (click)="onClick()">
       <div class="flex ph5" *ngIf="multi">
-        <div [class.selected]="multi" *ngFor="let item of selected;let i = index">
-          {{item[options.name]}}
-          <i *ngIf="multi && !readonly" class="iconfont icon-delete ml5" (click)="onDel(item, i)"></i>
-        </div>
-        <input *ngIf="selected.length" style="width:.1rem" >
-        <input *ngIf="!selected.length" class="full" placeholder="单击下拉，支持多选">
+        <ng-template ngFor let-item let-i="index" [ngForOf]="options.list">
+          <div [class.selected]="multi" *ngIf="isChecked(item)">
+            {{item[options.name]}}
+            <i *ngIf="!readonly" class="iconfont icon-delete ml5" (click)="onDel(item, i)"></i>
+          </div>
+        </ng-template>
+        <input *ngIf="!value.length" class="full" [placeholder]="placeholder">
+        <i class="iconfont icon-triangle-down right"></i>
       </div>
       <div class="flex middle" *ngIf="!multi">
         <input class="full pl10" [placeholder]="placeholder" [(ngModel)]="inputValue" (change)="onInputChange()"
-          [readOnly]="this.selected[0] && this.selected[0][this.options.id]"  (click)="onClickInput()">
-        <i *ngIf="!multi" class="iconfont icon-triangle-down right" (click)="showDropdown=true"></i>
+          [readOnly]="readonly || !customInput || inputReadonly"  (click)="onClickInput()">
+        <i class="iconfont icon-triangle-down right" (click)="showDropdown=true"></i>
       </div>
       <div class="dropdown" [class.block]="showDropdown" *ngIf="!readonly">
         <ng-template ngFor let-item let-i="index" [ngForOf]="options.list">
-          <div class="item" *ngIf="showItem(item)" (click)="onSelect(item)">{{item[options.name]}}</div>
+          <div class="item" *ngIf="!multi || !isChecked(item)" (click)="onSelect($event, item)">{{item[options.name]}}</div>
         </ng-template>
       </div>
     </div>
@@ -39,104 +41,119 @@ import { AbstractControl, ControlValueAccessor, NG_VALIDATORS, NG_VALUE_ACCESSOR
   }],
 })
 export class SelectComponent implements ControlValueAccessor, Validator {
-  selected: any[] = [];
-  val: any;
-  @Input() options: any = {id: 'id', name: 'name', list: []};
+
+  // 下拉列表选项，list表示下拉列表数组，其中：id表示value的来源，name表示显示来源
+  @Input() options: any = { id: 'id', name: 'name', list: [] };
+
+  // 是否多选
   @Input() multi: boolean = true;
+
+  // 是否支持自定义输入。true - 下拉选项id为空表示自定义输入
+  @Input() customInput: boolean = false;
+
   @Input() readonly: boolean = false;
   @Input() required: boolean = false;
   @Input() placeholder: string = '';
-  @Input() customInput: boolean = false;
-  valueChange: (value: any) => void = () => { };
+  @Output() change = new EventEmitter<any>();
+
+  private valueChange = (value: any) => { };
+  private touch = () => { };
+
+  // ngModeld的实际值
+  private _value: any;
+
+  // 下拉选项显示控制
   showDropdown: boolean;
+
+  // 单选显示数据
   inputValue: string;
+  // 单选输入框只读属性
+  inputReadonly: boolean;
+
   constructor(private _elemRef: ElementRef) {
   }
 
-  getSelected() {
-    this.inputValue = this.val;
-    if (!this.options.list) return;
-
-    for (let item of this.options.list) {
-      if (this.multi) {
-        if (!this.val || !this.val.length) return;
-        for (let v of this.val) {
-          if (v === item[this.options.id]) this.selected.push(item);
-        }
-      } else {
-        if (this.val === item[this.options.id]) this.selected.push(item);
-      }
-    }
+  ngOnInit() {
+    if (this.multi) this._value = [];
+    if (!this.placeholder) this.placeholder = this.multi ? '请选择 [可多选]' : '请选择';
   }
 
-  get value(): any { return this.val; };
+  get value(): any { return this._value; };
 
   set value(v: any) {
-    if (v && v !== this.val) {
-      this.valueChange(this.val);
-      this.getSelected();
+    if (v && v !== this._value) {
+      this._value = v;
+      if (!this.multi) this.inputValue = this._value;
     }
   }
 
-  validate(c: AbstractControl): {[key: string]: any} {
-    if (!this.val || (this.multi && this.val.length === 0)) {
+  validate(c: AbstractControl): { [key: string]: any } {
+    if (!this.required) return;
+
+    if (!this._value || (this.multi && this._value.length === 0)) {
       return {'required': true};
     }
   }
 
+  // model -> view
   writeValue(value: any) {
-    this.val = value;
-    this.getSelected();
+    this.value = value;
+    if (!this.multi && value && this.options.list) {
+      for (let item of this.options.list) {
+        if (value === item[this.options.id]) this.inputReadonly = true;
+      }
+    }
   }
 
+  // view -> model，当控件change后，调用的函数通知改变model
   registerOnChange(fn: any) {
     this.valueChange = fn;
   }
 
+  // 通知touched调用的函数
   registerOnTouched(fn: any) {
+    this.touch = fn;
   }
 
-  showItem(option: any): boolean {
-    if (!this.val || !this.multi) return true;
+  onSelect(event: any, item: any) {
+    event.stopPropagation();
 
-    let show = true;
-    for (let item of this.selected) {
-      if (item[this.options.id] === option[this.options.id]) {
-        show = false;
-        break;
+    this.showDropdown = false;
+
+    if (this.multi) {
+      if (!this._value) this._value = [];
+      this._value.push(item[this.options.id]);
+    } else {
+      this._value = item[this.options.id];
+
+      // [customInput自定义输入前提下]id为空表示自定义输入
+      if (this._value || !this.customInput) {
+        this.inputValue = item[this.options.name];
+        this.inputReadonly = true;
+      }
+      else {
+        this.inputValue = '';
+        this.inputReadonly = false;
       }
     }
-    return show;
-  }
-
-  onSelect(item: any) {
-    this.showDropdown = false;
-    if (this.multi) {
-      if (!this.val) this.val = [];
-      this.selected.push(item);
-      this.val.push(item[this.options.id]);
-    } else {
-      this.selected = [item];
-      this.val = item[this.options.id];
-      if (this.val || !this.customInput) this.inputValue = item[this.options.name];
-      else this.inputValue = '';
-    }
-    this.valueChange(this.val);
+    this.valueChange(this._value);
+    this.touch();
+    this.change.emit(this._value);
   }
 
   onInputChange() {
-    this.selected = [this.inputValue];
-    this.val = this.inputValue;
-    this.valueChange(this.val);
+    this.value = this.inputValue;
+    this.valueChange(this.inputValue);
+    this.change.emit(this.inputValue);
   }
 
   onDel(item: any, index: number) {
-    for (let i = 0; i < this.val.length; ++i) {
-      if (this.val[i] === item[this.options.id]) this.val.splice(i, 1);
+    for (let i = 0; i < this._value.length; ++i) {
+      if (this._value[i] === item[this.options.id]) this._value.splice(i, 1);
     }
 
-    this.selected.splice(index ,1);
-    this.valueChange(this.val);
+    this.valueChange(this._value);
+    this.change.emit(this._value);
   }
 
   onClick() {
@@ -144,12 +161,21 @@ export class SelectComponent implements ControlValueAccessor, Validator {
   }
 
   onClickInput() {
-    if (this.selected[0] && this.selected[0][this.options.id]) this.showDropdown = true;
+    if (!this.customInput || this.inputReadonly) this.showDropdown = true;
   }
 
   onClickDocument(event) {
     if (!this._elemRef.nativeElement.contains(event.target)) {
       this.showDropdown = false;
+    }
+  }
+
+  isChecked(item: any) {
+    if (!this.multi) return this._value === item[this.options.id];
+
+    if (!this._value || !this._value.length) return false;
+    for (let v of this._value) {
+      if (v === item[this.options.id]) return true;
     }
   }
 }
