9e232fecce2594ad1aeb55b1b255be119ea584c7
Alsmile <alsmile123@qq.com>
2017-09-08 14:05:40 +0800

    ui-loading

--- src/loading/ui.loading.component.ts
+++ src/loading/ui.loading.component.ts
@@ -2,9 +2,10 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'ui-loading',
-  template: `<div class="text-center" style="height: 100%"><div class="full"><i class="iconfont icon-loading icon-spin"></i>&nbsp;Loading... </div></div>`
+  template: `<div class="text-center" style="height: 100%"><div class="full"><i class="iconfont icon-loading icon-spin"></i>&nbsp; {{tip}}</div></div>`
 })
 export class UiLoadingComponent {
+  @Input() tip: string = 'Loading...';
   constructor() {
   }
 }
