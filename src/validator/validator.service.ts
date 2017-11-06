fcac05afbced8ba8142efa0e50c27be64df30839
Alsmile <alsmile123@qq.com>
2017-11-06 18:59:36 +0800

    fix bug: url validate

--- src/validator/validator.service.ts
+++ src/validator/validator.service.ts
@@ -23,11 +23,13 @@ export class ValidatorService {
 
   isPhone(value?: any, locale?: any): boolean {
     let pattern = this.phones[locale] || this.phones['zh-CN'];
-    return (new RegExp(pattern)).test(value);
+    return new RegExp(pattern).test(value);
   }
 
   isEmail(value?: any): boolean {
-    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(value);
+    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(
+      value
+    );
   }
 
   isPassword(value?: any): boolean {
@@ -39,16 +41,17 @@ export class ValidatorService {
   }
 
   isUrl(value?: any): boolean {
-    let strRegex = '^((https|http|ftp|rtsp|mms)?://)'
-      + '?(([0-9a-z_!~*\'().&=+$%-]+: )?[0-9a-z_!~*\'().&=+$%-]+@)?' //ftp的user@
-      + '(([0-9]{1,3}.){3}[0-9]{1,3}' // IP形式的URL- 199.194.52.184
-      + '|' // 允许IP和DOMAIN（域名）
-      + '([0-9a-z_!~*\'()-]+.)*' // 域名- www.
-      + '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' // 二级域名
-      + '[a-z]{2,6})' // first level domain- .com or .museum
-      + '(:[0-9]{1,4})?' // 端口- :80
-      + '((/?)|' // a slash isn't required if there is no file name
-      + '(/[0-9a-z_!~*\'().;?:@&=+$,%#-]+)+/?)$';
+    let strRegex =
+      '^((https|http|ftp|rtsp|mms)?://)' +
+      "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" + //ftp的user@
+      '(([0-9]{1,3}.){3}[0-9]{1,3}' + // IP形式的URL- 199.194.52.184
+      '|' + // 允许IP和DOMAIN（域名）
+      "([0-9a-z_!~*'()-]+.)*" + // 域名- www.
+      '([0-9a-z][0-9a-z-]{0,61})?[0-9a-z].' + // 二级域名
+      '[a-z]{2,6})' + // first level domain- .com or .museum
+      '(:[0-9]{1,4})?' + // 端口- :80
+      '((/?)|' + // a slash isn't required if there is no file name
+      "(/[0-9a-zA-Z_!~*'().;?:@&=+$,%#-]+)+/?)$";
 
     return new RegExp(strRegex).test(value);
   }
